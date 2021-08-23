2e669816328cf3a1247e1b676858aae124296477
eduardo1199 <eduardosooares456@hotmail.com>
2021-08-23 08:12:16 -0300

    paginação em postes

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -2,38 +2,62 @@ import { useEffect, useState } from "react";
 import './app.css';
 import { Postprops } from '../../types/Postprops';
 import Posts from '../../components/posts/Posts';
+import Button from '@material-ui/core/Button';
+
 
 function App() {
 
   const [posts, setPosts] = useState<Array<Postprops>>([]);
-
-  useEffect(() => {
-    postLoading();
-  }, [])
-
-  async function postLoading(){
-    const postsRespose = fetch("https://jsonplaceholder.typicode.com/posts");
-    const imagsPostReponse = fetch("https://jsonplaceholder.typicode.com/photos");
-
-    const [posts] = await Promise.all([postsRespose]);
-    const [imgsPost] = await Promise.all([imagsPostReponse]);
-
-    const photosJson = await imgsPost.json();
-    const postJson = await posts.json();
-
-    const postAndPhotos: Array<Postprops> = postJson.map((post: Postprops,index: number) => {
-       return {...post, url: photosJson[index].url};
-    })
-
-    setPosts(postAndPhotos);
+  const [homePage, setHomePage] = useState<number>(0);
+  const [lastPage, setLastPage] = useState<number>(5);
+  const [totalPage, setTotalPage] = useState<number>(0);
+  /* const [disableNext, setDisableNext] = useState<boolean>(false);
+  const [previousDisable, setPreviousDisable] = useState<boolean>(true);
+ */
+  const nextPage = () => {
+    if(homePage >= totalPage - lastPage){
+      return;
+    }
+    setHomePage(prev => prev + lastPage);
+    setLastPage(prev => prev + lastPage);
   }
 
+  const previousPage = () => {
+    if(homePage < 5){
+      return;
+    }
+    setHomePage(prev => prev - 5);
+    setLastPage(prev => prev - 5);
+   
+  }
+
+
+  useEffect(() => {
+    const postLoading  = async () => {
+      const postsRespose = fetch("https://jsonplaceholder.typicode.com/posts");
+      const imagsPostReponse = fetch("https://jsonplaceholder.typicode.com/photos");
   
+      const [posts] = await Promise.all([postsRespose]);
+      const [imgsPost] = await Promise.all([imagsPostReponse]);
+  
+      const photosJson = await imgsPost.json();
+      const postJson = await posts.json();
+  
+      const postAndPhotos: Array<Postprops> = postJson.map((post: Postprops,index: number) => {
+         return {...post, url: photosJson[index].url};
+      })
+
+      setTotalPage(postAndPhotos.length);
+      setPosts(postAndPhotos.slice(homePage, lastPage));
+    }
+    postLoading();
+  }, [homePage, lastPage]);
+
   return (
-    <div className="container-post">
-      {posts.map((post: Postprops, index: number) => {
-        return(
-          <>
+    <div>
+      <div className="container-post">
+        {posts.map((post: Postprops, index: number) => {
+          return(
             <Posts 
               key={index}
               id={post.id}
@@ -41,10 +65,27 @@ function App() {
               body={post.body}
               title={post.title}
             />
-          </>
-        )
-      })}
-    </div>
+          )
+        })}
+      </div>
+      <div className="container-buttons">
+        <Button 
+          variant="contained" 
+          onClick={() => previousPage()} 
+          disabled={homePage < 5 ? true : false}
+        >
+          Página anterior
+        </Button>
+        <Button 
+          variant="contained" 
+          color="primary" 
+          onClick={() => nextPage()}
+          disabled={homePage >= totalPage - lastPage ? true : false}
+        >
+          Próxima página
+        </Button>
+      </div>
+   </div>
   );
 }
 
